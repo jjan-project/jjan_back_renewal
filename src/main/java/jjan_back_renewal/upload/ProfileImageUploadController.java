f0b28750ab0e0afa3d24c0e6030c0ad6e47dad81
ParangBird <seekerman@naver.com>
2023-06-04 03:14:21 +0900

    [Fix] 프로필 이미지 업로드 기능 버그 수정

--- src/main/java/jjan_back_renewal/upload/ProfileImageUploadController.java
+++ src/main/java/jjan_back_renewal/upload/ProfileImageUploadController.java
@@ -1,9 +1,11 @@
 package jjan_back_renewal.upload;
 
-import jjan_back_renewal.user.entitiy.UserEntity;
+import jakarta.servlet.http.HttpServletRequest;
+import jjan_back_renewal.join.auth.JwtProvider;
 import jjan_back_renewal.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,27 +16,13 @@ import java.io.IOException;
 @RequestMapping("/api/user")
 public class ProfileImageUploadController {
     private final FileUploadService fileUploadService;
-    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
 
-    //유저 프로필 업로드
     @PostMapping("/set-profile-image")
-    public ResponseEntity<FileUploadResponseDto> uploadProfileImage(
-            @RequestParam("profilePhoto") MultipartFile multipartFile) throws IOException {
-        UserEntity user = saveEntityByEmail("test@naver.com");
-        FileUploadResponseDto profileUploadResponse = fileUploadService.uploadProfileImage(user.getEmail(), multipartFile);
+    public ResponseEntity<FileUploadResponseDto> uploadProfileImage(HttpServletRequest request,
+                                                                    @RequestParam("profilePhoto") MultipartFile multipartFile) throws IOException {
+        String email = jwtProvider.getUserEmail(request);
+        FileUploadResponseDto profileUploadResponse = fileUploadService.uploadProfileImage(email, multipartFile);
         return ResponseEntity.ok(profileUploadResponse);
-    }
-
-    UserEntity saveEntityByEmail(String email) {
-        return userRepository.save(UserEntity.builder()
-                .email(email)
-                .nickName("nickname")
-                .name("testname")
-                .gender("M")
-                .birth("19980830")
-                .password("pwd")
-                .address("SEOUL")
-                .profile("")
-                .build());
     }
 }
