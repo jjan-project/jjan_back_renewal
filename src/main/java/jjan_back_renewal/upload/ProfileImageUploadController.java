1e4e702d570d1593eb04be823969f4acd5ce344e
ParangBird <seekerman@naver.com>
2023-06-05 22:25:38 +0900

    [Refactor] 프로필 이미지 업로드 컨트롤러 RequestParam 리네이밍

--- src/main/java/jjan_back_renewal/upload/ProfileImageUploadController.java
+++ src/main/java/jjan_back_renewal/upload/ProfileImageUploadController.java
@@ -2,10 +2,8 @@ package jjan_back_renewal.upload;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jjan_back_renewal.join.auth.JwtProvider;
-import jjan_back_renewal.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,7 +18,7 @@ public class ProfileImageUploadController {
 
     @PostMapping("/set-profile-image")
     public ResponseEntity<FileUploadResponseDto> uploadProfileImage(HttpServletRequest request,
-                                                                    @RequestParam("profilePhoto") MultipartFile multipartFile) throws IOException {
+                                                                    @RequestParam("profileImage") MultipartFile multipartFile) throws IOException {
         String email = jwtProvider.getUserEmail(request);
         FileUploadResponseDto profileUploadResponse = fileUploadService.uploadProfileImage(email, multipartFile);
         return ResponseEntity.ok(profileUploadResponse);
