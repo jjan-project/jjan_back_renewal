243db5325fb4a6443460cc2162c3ba1dfadd2ed3
Limworld98 <phoenix1228@daum.net>
2023-06-19 15:13:12 +0900

    [Add] PostEntity, BaseTimeEntity
    -Config에CreatedAt, UpdatedAt을 담은 Abstract한 BaseTimeEntity 추가
    -Post 패키지 설정, PostEntity 추가

--- src/main/java/jjan_back_renewal/JjanBackRenewalApplication.java
+++ src/main/java/jjan_back_renewal/JjanBackRenewalApplication.java
@@ -2,7 +2,9 @@ package jjan_back_renewal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class JjanBackRenewalApplication {
 
---
+++ src/main/java/jjan_back_renewal/post/entity/PostEntity.java
@@ -0,0 +1,28 @@
+package jjan_back_renewal.post.entity;
+
+import jakarta.persistence.*;
+import jjan_back_renewal.config.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "post")
+public class PostEntity extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+}
