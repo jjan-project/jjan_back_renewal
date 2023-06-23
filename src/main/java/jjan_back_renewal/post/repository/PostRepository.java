c0462ecd518c5d30b28bcfa8951c9583d3eecd04
ParangBird <seekerman@naver.com>
2023-06-23 19:22:39 +0900

    [Delete] 기존 Post 관련 파일들 삭제

--- src/main/java/jjan_back_renewal/post/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package jjan_back_renewal.post.repository;
-
-import jjan_back_renewal.post.entity.PostEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<PostEntity,Long> {
-
-}
--- src/main/java/jjan_back_renewal/post/service/PostService.java
+++
@@ -1,8 +0,0 @@
-package jjan_back_renewal.post.service;
-
-import jjan_back_renewal.post.dto.PostCreateRequestDto;
-import jjan_back_renewal.post.dto.PostDto;
-
-public interface PostService {
-    PostDto write(String userEmail, PostCreateRequestDto postDto);
-}
