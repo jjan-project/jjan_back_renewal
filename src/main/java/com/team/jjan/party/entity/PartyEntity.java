dad2106817e8266524876a97f032d3a62f84b7bc
kkangh00n <kig2454@gmail.com>
2023-07-20 15:07:41 +0900

    fix: merge conflict 해결

--- src/main/java/com/team/jjan/party/entity/PartyEntity.java
+++ src/main/java/com/team/jjan/party/entity/PartyEntity.java
@@ -1,9 +1,8 @@
 package com.team.jjan.party.entity;
 
-import com.team.jjan.config.BaseTimeEntity;
+import jakarta.persistence.*;
 import com.team.jjan.party.dto.PartyUpdateRequestDto;
 import com.team.jjan.user.entitiy.UserEntity;
-import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.ArrayList;
