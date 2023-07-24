80238fef9af6a7412cc3b6a7eb55bb8bd48c4965
kkangh00n <kig2454@gmail.com>
2023-07-24 15:34:49 +0900

    fix: @LogIn 어노테이션 쿠키 기반 동작 수정

--- src/main/java/com/team/jjan/common/CurrentUserArgumentResolver.java
+++ src/main/java/com/team/jjan/common/CurrentUserArgumentResolver.java
@@ -5,6 +5,7 @@ import com.team.jjan.common.dto.CurrentUser;
 import com.team.jjan.jwt.support.JwtProvider;
 import com.team.jjan.user.exception.NoSuchEmailException;
 import com.team.jjan.user.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
@@ -32,8 +35,12 @@ public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolve
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        String email = jwtProvider.getUserEmail(request);
-        return CurrentUser.of(userRepository.findByEmail(email)
-                .orElseThrow(() -> new NoSuchEmailException(email)));
+
+        Cookie[] cookies = request.getCookies();
+        String accessToken = Arrays.stream(cookies).filter(c -> c.getName().equals("accessToken")).findAny()
+                .orElseThrow().getValue();
+        String userEmail = jwtProvider.getUserEmail(accessToken);
+        return CurrentUser.of(userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new NoSuchEmailException("사용자 정보를 찾을 수 없습니다.")));
     }
 }
