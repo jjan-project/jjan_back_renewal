0f3f350171de2d1b6df6afcb84ff8833ce0772e7
kkangh00n <kig2454@gmail.com>
2023-07-24 15:27:07 +0900

    fix: merge conflict 수정

--- src/main/java/com/team/jjan/common/CurrentUserArgumentResolver.java
+++ src/main/java/com/team/jjan/common/CurrentUserArgumentResolver.java
@@ -5,7 +5,6 @@ import com.team.jjan.common.dto.CurrentUser;
 import com.team.jjan.jwt.support.JwtProvider;
 import com.team.jjan.user.exception.NoSuchEmailException;
 import com.team.jjan.user.repository.UserRepository;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
@@ -14,8 +13,6 @@ import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-
-import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
@@ -35,12 +32,9 @@ public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolve
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String email = jwtProvider.getUserEmail(request);
 
-        Cookie[] cookies = request.getCookies();
-        String accessToken = Arrays.stream(cookies).filter(c -> c.getName().equals("accessToken")).findAny()
-                .orElseThrow().getValue();
-        String userEmail = jwtProvider.getUserEmail(accessToken);
-        return CurrentUser.of(userRepository.findByEmail(userEmail)
+        return CurrentUser.of(userRepository.findByEmail(email)
                 .orElseThrow(() -> new NoSuchEmailException("사용자 정보를 찾을 수 없습니다.")));
     }
 }
--- src/main/java/com/team/jjan/config/WebConfig.java
+++ src/main/java/com/team/jjan/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.team.jjan.config;
 
 import com.team.jjan.common.CurrentUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
