8cf94420ef7a672750b2f8a20266cebe64f85851
kkangh00n <kig2454@gmail.com>
2023-08-25 14:40:11 +0900

    feat: 회원가입 요청 DTO 기본 생성자 추가

--- src/main/java/com/team/jjan/join/dto/JoinRequest.java
+++ src/main/java/com/team/jjan/join/dto/JoinRequest.java
@@ -2,10 +2,12 @@ package com.team.jjan.join.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class JoinRequest {
     private String email;
