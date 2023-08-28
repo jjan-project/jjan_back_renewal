c194e0c0e213dd992f69ea1d0f18ef8b3364b178
Park s.j <follow2272@naver.com>
2023-08-28 22:09:32 +0900

    feat: 주소 정보 저장 API 구현 (#59)
    feat: 주소 정보 저장 API 구현

---
+++ src/main/java/com/team/jjan/user/dto/AddressRequest.java
@@ -0,0 +1,14 @@
+package com.team.jjan.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AddressRequest {
+
+    private String address;
+
+    private double locateX;
+
+    private double locateY;
+
+}
--- src/main/java/com/team/jjan/user/dto/JoinResponse.java
+++ src/main/java/com/team/jjan/user/dto/JoinResponse.java
@@ -14,6 +14,8 @@ public class JoinResponse {
     private String nickName;
     private String profile;
     private String address;
+    private double locateX;
+    private double locateY;
     private String gender;
     @JsonFormat(shape = JsonFormat.Shape.STRING , pattern = "yyyy/MM/dd" , timezone = "Asia/Seoul")
     private Date birth;
@@ -24,6 +26,8 @@ public class JoinResponse {
         this.nickName = userEntity.getNickName();
         this.profile = userEntity.getProfile();
         this.address = userEntity.getAddress();
+        this.locateX = userEntity.getLocateX();
+        this.locateY = userEntity.getLocateY();
         this.gender = userEntity.getGender();
         this.birth = userEntity.getBirth();
         this.drinkCapacity = userEntity.getDrinkCapacity();
