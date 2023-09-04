90c761969d6e33d4d142bf2d8dcee29bef1d9361
Park s.j <follow2272@naver.com>
2023-09-04 16:50:28 +0900

    fix: Cors 문제 해결 (#66)
    fix: Cors 문제 해결

--- src/main/java/com/team/jjan/config/WebConfig.java
+++ src/main/java/com/team/jjan/config/WebConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,8 +16,20 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final CurrentUserArgumentResolver currentUserArgumentResolver;
 
+    private final long MAX_AGE_SECS = 3600;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(currentUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://local.jjan.p-e.kr:3000" , "http://local.jjan.p-e.kr:5173")
+                .allowedMethods("GET","POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
 }
