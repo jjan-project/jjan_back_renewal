6dab660e32a5208c378563ccd18b86307db1d36f
Park s.j <follow2272@naver.com>
2023-09-06 20:59:04 +0900

    fix: Cors PATCH 메서드 추가 (#69)
    * fix: Cors PATCH 메서드 추가
    
    fix: Cors PATCH 메서드 추가

--- src/main/java/com/team/jjan/security/cors/CorsFilter.java
+++ src/main/java/com/team/jjan/security/cors/CorsFilter.java
@@ -22,7 +22,7 @@ public class CorsFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", origin);
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","GET, POST, OPTIONS, PUT, DELETE");
+        response.setHeader("Access-Control-Allow-Methods","GET, POST, OPTIONS, PUT, DELETE , PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
